ce24107976c62eb7d0fa3827f7295c790d0a602d
alexander1914 <alexander.oliveira99@gmail.com>
2025-01-13 20:00:09 +0000

    [main] - I did create a card-list with service part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,49 +1,39 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "../services/courses.service";
-import {map} from "rxjs/operators";
+import { Component, OnInit } from '@angular/core';
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { CoursesService } from "../services/courses.service";
+import { map } from "rxjs/operators";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: false
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: false
 })
 export class HomeComponent implements OnInit {
 
-    beginnerCourses$: Observable<Course[]>;
+  beginnerCourses$: Observable<Course[]>;
 
-    advancedCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
-    constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService) { }
 
-    }
+  ngOnInit() {
 
-    ngOnInit() {        
-        const courses$ = this.coursesService.findAllCourses();
-        console.log('Courses Observable:', courses$);
-        
-        this.beginnerCourses$ = courses$.pipe(
-            map(res => {
-              console.log('API Response:', res);
-              return res["payload"];
-            })
-        );
+    const courses$ = this.coursesService.findAllCourses();
 
-        this.beginnerCourses$ = courses$.pipe(
-            map(courses => {
-              console.log('Beginner Courses:', courses.filter(course => course.category === 'BEGINNER'));
-              return courses.filter(course => course.category === 'BEGINNER');
-            })
-          );
-          
-          this.advancedCourses$ = courses$.pipe(
-            map(courses => {
-              console.log('Advanced Courses:', courses.filter(course => course.category === 'ADVANCED'));
-              return courses.filter(course => course.category === 'ADVANCED');
-            })
-          );
-    }
+    this.beginnerCourses$ = courses$.pipe(
+      map(courses => courses.filter(course => course.category === 'BEGINNER'))
+    );
+
+    this.advancedCourses$ = courses$.pipe(
+      map(courses => courses.filter(course => course.category === 'ADVANCED'))
+    );
+
+  }
+
+  editCourse(course: Course) {
+
+  }
 
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,54 +1,45 @@
-
-
-import {Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
-import {Observable, throwError} from "rxjs";
-import {Course} from "../model/course";
-import {catchError, map} from "rxjs/operators";
-import {Lesson} from "../model/lesson";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
+import { map } from "rxjs/operators";
+import { Lesson } from "../model/lesson";
 
 
-@Injectable({
-    providedIn: 'root'
-  })
+@Injectable()
 export class CoursesService {
-    
-    private baseUrl = "http://localhost:9000"
 
-    constructor(private http:HttpClient) {
+    constructor(private http: HttpClient) {
 
     }
 
     findCourseById(courseId: number): Observable<Course> {
-        return this.http.get<Course>(this.baseUrl + `/courses/${courseId}`);
+        return this.http.get<Course>(`/api/courses/${courseId}`);
     }
 
     findAllCourses(): Observable<Course[]> {
-        return this.http.get(`${this.baseUrl}/courses`)
-          .pipe(
-            map(res => {
-              console.log('API Response:', res);
-              return res["payload"];
-            })
-          );
+        return this.http.get('/api/courses')
+            .pipe(
+                map(res => res['payload'])
+            );
     }
 
-    findAllCourseLessons(courseId:number): Observable<Lesson[]> {
-        return this.http.get(this.baseUrl + '/lessons', {
+    findAllCourseLessons(courseId: number): Observable<Lesson[]> {
+        return this.http.get('/api/lessons', {
             params: new HttpParams()
                 .set('courseId', courseId.toString())
                 .set('pageNumber', "0")
                 .set('pageSize', "1000")
         }).pipe(
-            map(res =>  res["payload"])
+            map(res => res["payload"])
         );
     }
 
     findLessons(
-        courseId:number, sortOrder = 'asc',
-        pageNumber = 0, pageSize = 3, sortColumn = 'seqNo'):  Observable<Lesson[]> {
+        courseId: number, sortOrder = 'asc',
+        pageNumber = 0, pageSize = 3, sortColumn = 'seqNo'): Observable<Lesson[]> {
 
-        return this.http.get(this.baseUrl + '/lessons', {
+        return this.http.get('/api/lessons', {
             params: new HttpParams()
                 .set('courseId', courseId.toString())
                 .set('sortOrder', sortOrder)
@@ -56,7 +47,7 @@ export class CoursesService {
                 .set('pageSize', pageSize.toString())
                 .set('sortColumn', sortColumn)
         }).pipe(
-            map(res =>  res["payload"])
+            map(res => res["payload"])
         );
     }
 
