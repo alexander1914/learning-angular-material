9241d2e49b4a816d4d4760c7b5bb93e5ac777736
alexander1914 <alexander.oliveira99@gmail.com>
2025-01-21 21:15:33 +0000

    [Navigation Container] - I did learn more about loading indicator with material

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -5,7 +5,7 @@ import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 import { Course } from "../model/course";
 import { CoursesService } from "../services/courses.service";
-import { debounceTime, distinctUntilChanged, startWith, tap, delay, catchError } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize } from 'rxjs/operators';
 import { merge, fromEvent, throwError } from "rxjs";
 import { Lesson } from '../model/lesson';
 
@@ -22,12 +22,14 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   lessons: Lesson[] = [];
 
+  loading: boolean = false;
+
   constructor(private route: ActivatedRoute,
     private coursesService: CoursesService) {
 
   }
 
-  displayedColumns = ['seqNo', 'description', 'duration']
+  displayedColumns = ['seqNo', 'description', 'duration'];
 
   ngOnInit() {
 
@@ -39,17 +41,18 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   loadLessonsPage() {
 
+    this.loading = true;
+
     this.coursesService.findLessons(this.course.id, "asc", 0, 3)
       .pipe(
         tap(lessons => this.lessons = lessons),
         catchError(err => {
-
           console.log("Error loading lessons", err);
           alert("Error loading lessons");
-
           return throwError(err)
 
-        })
+        }),
+        finalize(() => this.loading = false)
       )
       .subscribe();
   }
